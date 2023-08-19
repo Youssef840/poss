71004659235432b305c8dc41f50567bf0f5c8923
Youssef840 <140097174+Youssef840@users.noreply.github.com>
2023-08-19 14:13:13 +0300

    Update save_location.php

--- save_location.php
+++ save_location.php
@@ -1,8 +1,8 @@
 <?php
 $my_file=fopen("data.txt","w");
 $information="lat:". $_GET[" /n long"];
-fwrite($my_file,$information)
-fclose($my_file)
+fwrite($my_file,$information);
+fclose($my_file);
 
 
 >
